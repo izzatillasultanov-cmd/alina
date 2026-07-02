f3cf227a06d57683a9638f34f8af6a121c2b19b9
Gvardiya65921 <izzatillasultanov@gmail.com>
2026-07-02 20:58:51 +0500

    feat: Add user service.

---
+++ src/main/java/com/example/demo/service/UserServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public void save(User user) {
+        User user1 = userRepository.save(user);
+    }
+}
